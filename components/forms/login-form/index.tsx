213ecb8898de18b84e97711b98649214ceba88c9
Amitav Roy <reachme@amitavroy.com>
2021-05-01 12:29:10 +0530

    Added formik along with Yup for validation

--- components/forms/login-form/index.tsx
+++ components/forms/login-form/index.tsx
@@ -1,32 +1,62 @@
+import { Form, Formik, FormikHelpers } from 'formik';
+import { LoginSchema } from '../../../schemas/login.schema';
+import { ValidationMessage } from '../../validation-message';
+
 export const LoginForm = () => {
+  const handleLogin = (values, formikHelpers: FormikHelpers<any>) => {
+    console.log(values);
+  };
   return (
     <div className="p-2">
-      <form action="#">
-        <div className="mb-3">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your email address"
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <span className="me-3">
-            <button className="btn btn-success mr-3">Login</button>
-          </span>
-          <a href="#">Forgot password</a>
-        </div>
-      </form>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={handleLogin}
+        validationSchema={LoginSchema}
+      >
+        {({ values, handleChange, handleBlur, errors, touched }) => (
+          <Form>
+            <div className="mb-3">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                name="email"
+                value={values.email}
+                placeholder="Enter your email address"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="form-control"
+              />
+              {errors.email && touched.email ? (
+                <div className="validation-error">{errors.email}</div>
+              ) : null}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Enter your password"
+                className="form-control"
+              />
+              {errors.password && touched.password ? (
+                <div className="validation-error">{errors.password}</div>
+              ) : null}
+            </div>
+            <div className="mb-3">
+              <span className="me-3">
+                <button className="btn btn-success mr-3">Login</button>
+              </span>
+              <a href="#">Forgot password</a>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
