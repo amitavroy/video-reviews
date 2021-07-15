f0c51f9b602f9212afefbdfd2bae4406e3e59e19
Amitav Roy <reachme@amitavroy.com>
2021-07-15 21:49:32 +0530

    Added the drag and drop code back which got missed out.

--- pages/course/view/[id].tsx
+++ pages/course/view/[id].tsx
@@ -1,9 +1,39 @@
+import arrayMove from 'array-move';
 import React, { useEffect, useState } from 'react';
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+} from 'react-sortable-hoc';
 import ChapterAddForm from '../../../components/forms/chapter-add-form';
 
 import Layout from '../../../components/layout';
 import { ICourse } from '../../../contracts/ICourse';
 import CourseService from '../../../services/course.service';
+
+const DragHandle = SortableHandle(() => <i className="bi bi-arrows-move"></i>);
+
+const SortableChapter = SortableElement(({ chapter }) => {
+  return (
+    <div key={chapter.id}>
+      <div className="d-flex p-2 my-2 border">
+        <DragHandle /> <span className="ps-2">{chapter.name}</span>
+      </div>
+    </div>
+  );
+});
+
+const SortableCourse = SortableContainer(({ chapters }) => {
+  return (
+    <div>
+      {chapters.map((chapter, index) => {
+        return (
+          <SortableChapter chapter={chapter} key={chapter.id} index={index} />
+        );
+      })}
+    </div>
+  );
+});
 
 const CourseViewPage = ({ id }) => {
   useEffect(() => {
@@ -19,6 +49,11 @@ const CourseViewPage = ({ id }) => {
     newCourse.chapters.unshift(chapter);
     setCourse({ ...newCourse });
   };
+  const handleSort = ({ oldIndex, newIndex }) => {
+    const newCourse = course;
+    arrayMove.mutate(newCourse.chapters, oldIndex, newIndex);
+    setCourse({ ...newCourse });
+  };
   return (
     <Layout pageTitle="My courses">
       <div className="d-flex flex-column justify-content-center mb-3">
@@ -32,10 +67,13 @@ const CourseViewPage = ({ id }) => {
           </div>
           <div className="col-7 offset-md-1">
             <h3>Chapters</h3>
-            {course &&
-              course.chapters.map((chapter, index) => {
-                return <div key={index}>{chapter.name}</div>;
-              })}
+            {course && course.chapters.length > 0 && (
+              <SortableCourse
+                chapters={course.chapters}
+                onSortEnd={handleSort}
+                useDragHandle
+              />
+            )}
           </div>
         </div>
       </div>
