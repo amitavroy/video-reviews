e4ab35d5cc563042d9a2e31101e69795be2eb73a
Amitav Roy <reachme@amitavroy.com>
2021-07-15 11:08:12 +0530

    This was not commited. Build was failing

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import AuthService from './auth.service';
 import UrlService from './url.service';
 
 class UserService {
   static createUser = async (postData) => {
     const resp = await axios.post(UrlService.REGISTRATION_URL, postData);
     if (resp.status === 201) return true;
+  };
+  static getUserListingForAdmin = async () => {
+    const authHeaders = AuthService.getUserAuthHeader();
+    const resp = await axios.get(UrlService.ADMIN_USER_LIST, {
+      headers: authHeaders,
+    });
+    if (resp.status === 200) return resp;
   };
 }
 
